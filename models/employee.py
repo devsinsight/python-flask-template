1c1f9d89725e4feb5d5bb74ff9da207e8d59dd7f
Jose Luis Olivares Rojas <olivares.rojas.jose@gmail.com>
2017-10-19 09:37:07 -0500

    hire date added

--- models/employee.py
+++ models/employee.py
@@ -1,11 +1,12 @@
 
 class Employee:
 
-    def __init__(self, first_name, last_name, is_full_time, payment_type, primary_language):
+    def __init__(self, first_name, last_name, is_full_time, payment_type, primary_language, hire_date):
         self.id = 1
         self.first_name = first_name
         self.last_name = last_name
         self.is_full_time = is_full_time
         self.payment_type = payment_type
         self.primary_language = primary_language
+        self.hire_date = hire_date
 
